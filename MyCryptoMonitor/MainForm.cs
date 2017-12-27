795c72cd6906e845a12115fa7a800fbeb8099473
Sean Crowley <crowley.p.sean@gmail.com>
2017-12-26 23:26:54 -0600

    Addressed timeout

--- MyCryptoMonitor/MainForm.cs
+++ MyCryptoMonitor/MainForm.cs
@@ -27,7 +27,7 @@ namespace MyCryptoMonitor
                 new CoinConfig { coin = "BTC", bought =  0, paid = 0},
                 new CoinConfig { coin = "ETH", bought =  0, paid = 0},
                 new CoinConfig { coin = "LTC", bought =  0, paid = 0},
-                new CoinConfig { coin = "XRP", bought = (decimal) 608.79, paid = 650},
+                new CoinConfig { coin = "XRP", bought = (decimal) 630.592988, paid = 675},
                 new CoinConfig { coin = "XLM", bought = (decimal) 1238.73999, paid = 350}
             };
 
@@ -41,109 +41,125 @@ namespace MyCryptoMonitor
 
         private void LoadCoinCapData()
         {
-            //Display status
-            MethodInvoker invoke = delegate
+            try
             {
-                statusLabel.Text = "Status: Refreshing";
-            };
-            Invoke(invoke);
-
-            //Download coin data from CoinCap
-            var webClient = new WebClient();
-            var response = webClient.DownloadString("http://coincap.io/front");
-            var coins = JsonConvert.DeserializeObject<List<CoinData>>(response);
-
-            //Overall values
-            decimal totalPaid = 0;
-            decimal totalProfits = 0;
-
-            //Index of coin gui line
-            int index = 0;
-
-            //Loop through all coins from config
-            foreach(CoinConfig coin in _coinConfigs)
-            {
-                //Parse the selected coin
-                CoinData downloadedCoin = coins.Single(c => c.shortName == coin.coin);
-
-                //Configure the gui
-                if (_loadGuiLines)
+                //Display status
+                MethodInvoker invoke = delegate
                 {
-                    //Store the intial coin price at startup
-                    coin.StartupPrice = downloadedCoin.price;
+                    statusLabel.Text = "Status: Refreshing";
+                };
+                Invoke(invoke);
 
-                    //Create the gui line
-                    CoinGuiLine newLine = new CoinGuiLine(downloadedCoin.shortName, index);
+                //Download coin data from CoinCap
+                var webClient = new WebClient();
+                var response = webClient.DownloadString("http://coincap.io/front");
+                var coins = JsonConvert.DeserializeObject<List<CoinData>>(response);
 
-                    //Set the bought and paid amounts
-                    newLine.BoughtTextBox.Text = coin.bought.ToString();
-                    newLine.PaidTextBox.Text = coin.paid.ToString();
+                //Overall values
+                decimal totalPaid = 0;
+                decimal totalProfits = 0;
 
-                    //Add the line elements to gui
+                //Index of coin gui line
+                int index = 0;
+
+                //Loop through all coins from config
+                foreach(CoinConfig coin in _coinConfigs)
+                {
+                    //Parse the selected coin
+                    CoinData downloadedCoin = coins.Single(c => c.shortName == coin.coin);
+
+                    //Configure the gui
+                    if (_loadGuiLines)
+                    {
+                        //Store the intial coin price at startup
+                        coin.StartupPrice = downloadedCoin.price;
+
+                        //Create the gui line
+                        CoinGuiLine newLine = new CoinGuiLine(downloadedCoin.shortName, index);
+
+                        //Set the bought and paid amounts
+                        newLine.BoughtTextBox.Text = coin.bought.ToString();
+                        newLine.PaidTextBox.Text = coin.paid.ToString();
+
+                        //Add the line elements to gui
+                        invoke = delegate
+                        {
+                            Height += 25;
+                            Controls.Add(newLine.CoinLabel);
+                            Controls.Add(newLine.PriceLabel);
+                            Controls.Add(newLine.BoughtTextBox);
+                            Controls.Add(newLine.TotalLabel);
+                            Controls.Add(newLine.PaidTextBox);
+                            Controls.Add(newLine.ProfitLabel);
+                            Controls.Add(newLine.ChangeDollarLabel);
+                            Controls.Add(newLine.ChangePercentLabel);
+                        };
+                        Invoke(invoke);
+
+                        //Add line to list
+                        _coinGuiLines.Add(newLine);
+                    }
+
+                    //Get the gui line for coin
+                    CoinGuiLine line = (from c in _coinGuiLines where c.CoinName.Equals(downloadedCoin.shortName) select c).First();
+
+                    //Calculate
+                    decimal bought = Convert.ToDecimal(line.BoughtTextBox.Text);
+                    decimal paid = Convert.ToDecimal(line.PaidTextBox.Text);
+                    decimal total = bought * downloadedCoin.price;
+                    decimal profit = total - paid;
+                    decimal changeDollar = downloadedCoin.price - coin.StartupPrice;
+                    decimal changePercent = ((downloadedCoin.price - coin.StartupPrice) / coin.StartupPrice) * 100;
+
+                    //Add to totals
+                    totalPaid += paid;
+                    totalProfits += paid + profit;
+
+                    //Update gui
                     invoke = delegate
                     {
-                        Height += 25;
-                        Controls.Add(newLine.CoinLabel);
-                        Controls.Add(newLine.PriceLabel);
-                        Controls.Add(newLine.BoughtTextBox);
-                        Controls.Add(newLine.TotalLabel);
-                        Controls.Add(newLine.PaidTextBox);
-                        Controls.Add(newLine.ProfitLabel);
-                        Controls.Add(newLine.ChangeDollarLabel);
-                        Controls.Add(newLine.ChangePercentLabel);
+                        line.CoinLabel.Text = downloadedCoin.shortName;
+                        line.PriceLabel.Text = $"${downloadedCoin.price:0.00}";
+                        line.TotalLabel.Text = $"${total:0.00}";
+                        line.ProfitLabel.Text = $"${profit:0.00}";
+                        line.ChangeDollarLabel.Text = $"${changeDollar:0.000000}";
+                        line.ChangePercentLabel.Text = $"{changePercent:0.00}%";
                     };
                     Invoke(invoke);
 
-                    //Add line to list
-                    _coinGuiLines.Add(newLine);
+                    //Incremenet coin line index
+                    index++;
                 }
-
-                //Get the gui line for coin
-                CoinGuiLine line = (from c in _coinGuiLines where c.CoinName.Equals(downloadedCoin.shortName) select c).First();
-
-                //Calculate
-                decimal bought = Convert.ToDecimal(line.BoughtTextBox.Text);
-                decimal paid = Convert.ToDecimal(line.PaidTextBox.Text);
-                decimal total = bought * downloadedCoin.price;
-                decimal profit = total - paid;
-                decimal changeDollar = downloadedCoin.price - coin.StartupPrice;
-                decimal changePercent = ((downloadedCoin.price - coin.StartupPrice) / coin.StartupPrice) * 100;
-
-                //Add to totals
-                totalPaid += paid;
-                totalProfits += paid + profit;
 
                 //Update gui
                 invoke = delegate
                 {
-                    line.CoinLabel.Text = downloadedCoin.shortName;
-                    line.PriceLabel.Text = $"${downloadedCoin.price:0.00}";
-                    line.TotalLabel.Text = $"${total:0.00}";
-                    line.ProfitLabel.Text = $"${profit:0.00}";
-                    line.ChangeDollarLabel.Text = $"${changeDollar:0.000000}";
-                    line.ChangePercentLabel.Text = $"{changePercent:0.00}%";
+                    totalProfit.Text = $"${totalProfits:0.00}";
+                    totalProfitChange.Text = $"(${totalProfits - totalPaid:0.00})";
+                    statusLabel.Text = "Status: Sleeping";
+                    refreshLabel.Text = $"Refreshes: {_refreshCount}";
                 };
                 Invoke(invoke);
 
-                //Incremenet coin line index
-                index++;
+                //Sleep and rerun
+                _loadGuiLines = false;
+                _refreshCount++;
+                Thread.Sleep(5000);
+                LoadCoinCapData();
             }
-
-            //Update gui
-            invoke = delegate
+            catch(Exception)
             {
-                totalProfit.Text = $"${totalProfits:0.00}";
-                totalProfitChange.Text = $"(${totalProfits - totalPaid:0.00})";
-                statusLabel.Text = "Status: Sleeping";
-                refreshLabel.Text = $"Refreshes: {_refreshCount}";
-            };
-            Invoke(invoke);
+                //Display status
+                MethodInvoker invoke = delegate
+                {
+                    statusLabel.Text = "Status: Failed";
+                };
+                Invoke(invoke);
 
-            //Sleep and rerun
-            _loadGuiLines = false;
-            _refreshCount++;
-            Thread.Sleep(5000);
-            LoadCoinCapData();
+                //Sleep and rerun
+                Thread.Sleep(5000);
+                LoadCoinCapData();
+            }
         }
 
         private void Reset_Click(object sender, EventArgs e)
