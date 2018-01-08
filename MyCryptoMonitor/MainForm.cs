0634316ad8d6fa1b85f4fc44f136fc1c0b7c6588
Sean Crowley <crowley.p.sean@gmail.com>
2018-01-08 00:27:07 -0600

    Fixed reset

--- MyCryptoMonitor/MainForm.cs
+++ MyCryptoMonitor/MainForm.cs
@@ -147,138 +147,95 @@ namespace MyCryptoMonitor
                 totalProfits += paid + profit;
 
                 //Update gui
-                Invoke(new Action(() =>
-                {
-                    line.CoinLabel.Show();
-                    line.CoinLabel.Text = downloadedCoin.shortName;
-                    line.PriceLabel.Text = $"${downloadedCoin.price:0.00}";
-                    line.TotalLabel.Text = $"${total:0.00}";
-                    line.ProfitLabel.Text = $"${profit:0.00}";
-                    line.ChangeDollarLabel.Text = $"${changeDollar:0.000000}";
-                    line.ChangePercentLabel.Text = $"{changePercent:0.00}%";
-                    line.Change24HrPercentLabel.Text = $"{downloadedCoin.cap24hrChange}%";
-                }));
+                line.CoinLabel.Show();
+                line.CoinLabel.Text = downloadedCoin.shortName;
+                line.PriceLabel.Text = $"${downloadedCoin.price:0.00}";
+                line.TotalLabel.Text = $"${total:0.00}";
+                line.ProfitLabel.Text = $"${profit:0.00}";
+                line.ChangeDollarLabel.Text = $"${changeDollar:0.000000}";
+                line.ChangePercentLabel.Text = $"{changePercent:0.00}%";
+                line.Change24HrPercentLabel.Text = $"{downloadedCoin.cap24hrChange}%";
             }
 
             //Update gui
-            Invoke(new Action(() =>
-            {
-                totalProfit.Text = $"${totalProfits:0.00}";
-                totalProfitChange.Text = $"(${totalProfits - totalPaid:0.00})";
-                statusLabel.Text = "Status: Sleeping";
-                refreshLabel.Text = $"Refreshes: {_refreshCount}";
-            }));
+            totalProfit.Text = $"${totalProfits:0.00}";
+            totalProfitChange.Text = $"(${totalProfits - totalPaid:0.00})";
+            statusLabel.Text = "Status: Sleeping";
+            refreshLabel.Text = $"Refreshes: {_refreshCount}";
 
             //Sleep and rerun
             _loadGuiLines = false;
             _refreshCount++;
         }
 
-        //Setup coins
-        private delegate void SetupCoinsDelegate(string text);
-        public void SetupCoins(string text)
+        //Reset
+        private delegate void ReloadDelegate();
+        public void Reload()
         {
             if (InvokeRequired)
             {
-                Invoke(new SetupCoinsDelegate(SetupCoins), text);
+                Invoke(new ReloadDelegate(Reload));
+                return;
+            }
+            
+            //Set status
+            statusLabel.Text = "Status: Reloading";
+            _refreshCount = 0;
+
+            //Reset totals
+            totalProfit.Text = "$0.00";
+            totalProfitChange.Text = "($0.00)";
+
+            foreach (CoinConfig coin in _coinConfigs)
+            {
+                //Get the selected coin line
+                CoinGuiLine line = (from c in _coinGuiLines where c.CoinName.Equals(coin.coin) select c).First();
+
+                //Reset labels
+                line.PriceLabel.Text = "$0.00";
+                line.TotalLabel.Text = "$0.00";
+                line.ProfitLabel.Text = "$0.00";
+                line.ChangeDollarLabel.Text = "$0.000000";
+                line.ChangePercentLabel.Text = "0.00%";
+                line.Change24HrPercentLabel.Text = "0.00%";
+
+                //Reset inputs
+                line.BoughtTextBox.Text = coin.bought.ToString();
+                line.PaidTextBox.Text = coin.paid.ToString();
+            }
+        }
+
+        //Remove lines
+        private delegate void RemoveDelegate();
+        public void Remove()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new ReloadDelegate(Reload));
                 return;
             }
 
+            foreach (var coin in _coinGuiLines)
+            {
+                //Add the line elements to gui
+
+                //Height -= 25;
+                Controls.Remove(coin.CoinLabel);
+                Controls.Remove(coin.PriceLabel);
+                Controls.Remove(coin.BoughtTextBox);
+                Controls.Remove(coin.TotalLabel);
+                Controls.Remove(coin.PaidTextBox);
+                Controls.Remove(coin.ProfitLabel);
+                Controls.Remove(coin.ChangeDollarLabel);
+                Controls.Remove(coin.ChangePercentLabel);
+                Controls.Remove(coin.Change24HrPercentLabel);
+            }
+
+            _loadGuiLines = true;
         }
         #endregion
 
-        private void LoadCoinCapData()
-        {
-            try
-            {
-                //Display status
-                Invoke(new Action(() =>
-                {
-                    statusLabel.Text = "Status: Refreshing";
-                }));
-
-                //Download coin data from CoinCap
-                var webClient = new WebClient();
-                var response = webClient.DownloadString("http://coincap.io/front");
-
-                //Overall values
-                decimal totalPaid = 0;
-                decimal totalProfits = 0;
-
-                //Index of coin gui line
-                int index = 0;
-
-                //Loop through all coins from config
-                foreach(CoinConfig coin in _coinConfigs)
-                {
-                    var coins = JsonConvert.DeserializeObject<List<CoinData>>(response);
-                    CoinData downloadedCoin = coins.Single(c => c.shortName == coin.coin);
-
-                    if (_loadGuiLines)
-                        AddCoin(coin, downloadedCoin, index);
-
-                    //Incremenet coin line index
-                    index++;
-
-                    //Get the gui line for coin
-                    CoinGuiLine line = (from c in _coinGuiLines where c.CoinName.Equals(downloadedCoin.shortName) select c).First();
-
-                    //Calculate
-                    decimal bought = Convert.ToDecimal(line.BoughtTextBox.Text);
-                    decimal paid = Convert.ToDecimal(line.PaidTextBox.Text);
-                    decimal total = bought * downloadedCoin.price;
-                    decimal profit = total - paid;
-                    decimal changeDollar = downloadedCoin.price - coin.StartupPrice;
-                    decimal changePercent = ((downloadedCoin.price - coin.StartupPrice) / coin.StartupPrice) * 100;
-
-                    //Add to totals
-                    totalPaid += paid;
-                    totalProfits += paid + profit;
-
-                    //Update gui
-                    Invoke(new Action(() =>
-                    {
-                        line.CoinLabel.Show();
-                        line.CoinLabel.Text = downloadedCoin.shortName;
-                        line.PriceLabel.Text = $"${downloadedCoin.price:0.00}";
-                        line.TotalLabel.Text = $"${total:0.00}";
-                        line.ProfitLabel.Text = $"${profit:0.00}";
-                        line.ChangeDollarLabel.Text = $"${changeDollar:0.000000}";
-                        line.ChangePercentLabel.Text = $"{changePercent:0.00}%";
-                        line.Change24HrPercentLabel.Text = $"{downloadedCoin.cap24hrChange}%";
-                    }));
-                }
-
-                //Update gui
-                Invoke(new Action(() =>
-                {
-                    totalProfit.Text = $"${totalProfits:0.00}";
-                    totalProfitChange.Text = $"(${totalProfits - totalPaid:0.00})";
-                    statusLabel.Text = "Status: Sleeping";
-                    refreshLabel.Text = $"Refreshes: {_refreshCount}";
-                }));
-
-                //Sleep and rerun
-                _loadGuiLines = false;
-                _refreshCount++;
-                Thread.Sleep(5000);
-                LoadCoinCapData();
-            }
-            catch(Exception e)
-            {
-                //Display status
-                MethodInvoker invoke = delegate
-                {
-                    statusLabel.Text = $"Status: Failed - {e.Message}";
-                };
-                Invoke(invoke);
-
-                //Sleep and rerun
-                Thread.Sleep(5000);
-                LoadCoinCapData();
-            }
-        }
-
+        #region Methods
         private void AddCoin(CoinConfig coin, CoinData downloadedCoin, int index)
         {
             //Store the intial coin price at startup
@@ -310,45 +267,14 @@ namespace MyCryptoMonitor
             //Add line to list
             _coinGuiLines.Add(newLine);
         }
+        #endregion
 
         #region Events
         private void Reset_Click(object sender, EventArgs e)
         {
-            //Set status
-            statusLabel.Text = "Status: Reset";
-            _refreshCount = 0;
-
-            //Reset totals
-            totalProfit.Text = "$0.00";
-            totalProfitChange.Text = "($0.00)";
-
-            MethodInvoker invoke = delegate
-            {
-                //Download coin data from CoinCap
-                var webClient = new WebClient();
-                var response = webClient.DownloadString("http://coincap.io/front");
-                var coins = JsonConvert.DeserializeObject<List<CoinData>>(response);
-
-                foreach (CoinConfig coin in _coinConfigs)
-                {
-                    //Get the selected coin line
-                    CoinGuiLine line = (from c in _coinGuiLines where c.CoinName.Equals(coin.coin) select c).First();
-
-                    //Reset labels
-                    line.ProfitLabel.Text = $"$0.00";
-                    line.ChangeDollarLabel.Text = $"$0.000000";
-                    line.ChangePercentLabel.Text = $"0.00%";
-
-                    //Parse the selected coin
-                    CoinData downloadedCoin = coins.Single(c => c.shortName == coin.coin);
-
-                    //Reset elements
-                    coin.StartupPrice = downloadedCoin.price;
-                    line.BoughtTextBox.Text = coin.bought.ToString();
-                    line.PaidTextBox.Text = coin.paid.ToString();
-                }
-            };
-            Invoke(invoke);
+            //Update refreshes
+            ReloadDelegate reload = new ReloadDelegate(Reload);
+            BeginInvoke(reload);
         }
 
         private void Exit_Click(object sender, EventArgs e)
@@ -402,6 +328,11 @@ namespace MyCryptoMonitor
 
         private void RemoveCoin_Click(object sender, EventArgs e)
         {
+            //Update refreshes
+            RemoveDelegate remove = new RemoveDelegate(Remove);
+            BeginInvoke(remove);
+
+            /*
             InputForm form = new InputForm();
             form.SetSubmitLabel("Remove");
             var result = form.ShowDialog();
@@ -414,6 +345,7 @@ namespace MyCryptoMonitor
                 _coinConfigs.RemoveAll(x => x.coin.Equals(form.InputText.ToUpper()));
                 ReloadForm();
             }
+            */
         }
         
         //Load portfolios
