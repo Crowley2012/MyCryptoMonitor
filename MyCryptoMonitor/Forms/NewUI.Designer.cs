e6713595d9ba13d459754fa1475ae6e1cbc6ac8d
Sean Crowley <crowley.p.sean@gmail.com>
2018-01-31 16:49:36 -0600

    Working on new UI

---
+++ MyCryptoMonitor/Forms/NewUI.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MyCryptoMonitor
+{
+    partial class NewUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Location = new System.Drawing.Point(12, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(192, 201);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Location = new System.Drawing.Point(12, 316);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(192, 201);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Teal;
+            this.panel3.Location = new System.Drawing.Point(210, 316);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(192, 201);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Teal;
+            this.panel4.Location = new System.Drawing.Point(210, 109);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(192, 201);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Teal;
+            this.panel5.Location = new System.Drawing.Point(408, 109);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(192, 201);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(182)))), ((int)(((byte)(218)))));
+            this.panel6.Location = new System.Drawing.Point(606, 109);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(192, 201);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Teal;
+            this.panel7.Location = new System.Drawing.Point(408, 316);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(192, 201);
+            this.panel7.TabIndex = 2;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Teal;
+            this.panel8.Location = new System.Drawing.Point(606, 316);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(192, 201);
+            this.panel8.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Teal;
+            this.panel9.Location = new System.Drawing.Point(12, 12);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(786, 78);
+            this.panel9.TabIndex = 1;
+            // 
+            // NewUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(810, 529);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "NewUI";
+            this.Text = "NewUI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+    }
+}
---
+++ MyCryptoMonitor/Forms/NewUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MyCryptoMonitor
+{
+    public partial class NewUI : Form
+    {
+        public NewUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
